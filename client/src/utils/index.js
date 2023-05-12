5a0a1c0a1d2ff62ec906af446dad601ff313cbe6
Ayush <ydvayush06@gmail.com>
2023-05-12 22:08:34 +0530

    first commit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,9 +1,14 @@
 export const daysLeft = (deadline) => {
-    const difference = new Date(deadline).getTime() - Date.now();
-    const remainingDays = difference / (1000 * 3600 * 24);
+  const difference = new Date(deadline).getTime() - Date.now();
+  const remainingDays = difference / (1000 * 3600 * 24);
   
+  if (remainingDays <= 0) {
+    return "Campaign expired ";
+  } else {
     return remainingDays.toFixed(0);
-  };
+  }
+};
+
   
   export const calculateBarPercentage = (goal, raisedAmount) => {
     const percentage = Math.round((raisedAmount * 100) / goal);
